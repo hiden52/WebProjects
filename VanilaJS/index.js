208d7eac75fdfd6f3cda65e412f0b48752f928d7
정상훈 <20150599@office.bufs.ac.kr>
2020-12-28 11:29:19 +0000

    JS is not fun

---
+++ VanilaJS/index.js
@@ -0,0 +1,16 @@
+console.log('I\'m working');
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fir", "Sat", "Sun"];
+
+const Jack = {
+    name: "Jack",
+    age: 32,
+    gender: "Male",
+    isStudent: false
+};
+
+console.log(daysOfWeek);
+console.log(daysOfWeek[2]);
+console.log(Jack.name);
+Jack.name = "Jack Jack";
+console.log(Jack.name);
+console.log(Jack);
