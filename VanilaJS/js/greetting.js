221bf69c8b3fb5f2e3c1e393e66a1ab2697099bb
정상훈 <20150599@office.bufs.ac.kr>
2020-12-29 11:31:41 +0000

    Creating clock app

---
+++ VanilaJS/js/greetting.js
@@ -0,0 +1,24 @@
+const form = document.querySelector(".js-form"),
+    input = document.querySelector("input");
+
+const User_LS = "currentUser";
+
+function loadName()
+{
+    const currentUser = localStorage.getItem(User_LS);
+    if(currentUser == null)
+    {
+        
+    }
+    else
+    {
+
+    }
+
+}
+
+function init()
+{
+
+}
+init();
